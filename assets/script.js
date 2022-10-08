9ec24c23cc05303103dda9519936d5127afe55d1
prkrgreenwell <prkrgreenwell@gmail.com>
2022-10-08 13:29:09 -0600

    added opening screen

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var question = document.querySelector("#question");
+var answerA = document.querySelector("#answer-a");
+var answerB = document.querySelector("#answer-b");
+var answerC = document.querySelector("#answer-c");
+var answerD = document.querySelector("#answer-d");
