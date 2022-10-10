7aeb34296abb093149fe305fecf4286c1a060eb7
prkrgreenwell <prkrgreenwell@gmail.com>
2022-10-09 23:48:45 -0600

    started a timer, but I'm tired so I'm gonna go to bed now

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,32 @@
+// Set variables for the different screens
+var openingScreen = document.getElementById("opening-screen");
+var quizScreen = document.getElementById("quiz-screen");
+var highScoreScreen = document.getElementById("high-score-screen");
+
 // Set variables for all the buttons we will be using
+var beginQuiz = document.querySelector("#start-button");
+var viewScoresLink = document.getElementById("view-high-scores");
 
-var question = document.querySelector("#question");
-var answerA = document.querySelector("#answer-a");
-var answerB = document.querySelector("#answer-b");
-var answerC = document.querySelector("#answer-c");
-var answerD = document.querySelector("#answer-d");
+// Set up the timer
+var count = 10;
+if (count > 0) {
+  setInterval(countDown, 1000);
+}
+function countDown() {
+  console.log(count);
+  count--;
+}
 
-var openToQuiz = document
-  .getElementById("start-button")
-  .addEventListener("click", function (event) {
-    console.log("hey");
-    document.getElementById("opening-screen").style.display = "none";
-    document.getElementById("view-high-scores").style.visibility = "hidden";
-    document.getElementById("quiz-screen").style.display = "flex";
-  });
+// Moves from beginning screen and starts the quiz
+beginQuiz.addEventListener("click", (e) => {
+  openingScreen.style.display = "none";
+  viewScoresLink.style.visibility = "hidden";
+  quizScreen.style.display = "flex";
+});
+
+// Moves from start screen to high score page
+viewScoresLink.addEventListener("click", (e) => {
+  openingScreen.style.display = "none";
+  viewScoresLink.style.visibility = "hidden";
+  highScoreScreen.style.display = "flex";
+});
