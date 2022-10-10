8ee2b599ca0abcc5fac8940bd0ed8bc9953c0517
prkrgreenwell <prkrgreenwell@gmail.com>
2022-10-10 17:30:47 -0600

    timer is set up well, moving from screen to screen and stuff

--- assets/script.js
+++ assets/script.js
@@ -2,36 +2,49 @@
 var openingScreen = document.getElementById("opening-screen");
 var quizScreen = document.getElementById("quiz-screen");
 var highScoreScreen = document.getElementById("high-score-screen");
+var endScreen = document.getElementById("end-screen");
 
 // Set variables for all the buttons we will be using
 var beginQuiz = document.querySelector("#start-button");
 var viewScoresLink = document.getElementById("view-high-scores");
-
-// Set up the timer
-//TO-DO get timer to stop at 0
-//TO-DO display timer on screen
-//TO-DO time penalty
-//TO-DO move to end screen when time runs out
-var timeLeft = 10;
 var time = document.getElementById("time");
 
-var timerId = setInterval(countDown, 1000);
+// timer function: called when moved to quiz screen
+function startTimer() {
+  //TO-DO time penalty
+  var timeLeft = 10;
 
-function countDown() {
-  if (timeLeft >= 0) {
-    time.innerHTML = "Time: " + timeLeft;
-    timeLeft--;
-  } else {
-    clearTimeout;
-    //insert breakpoint here
+  var timerId = setInterval(countDown, 1000);
+
+  function countDown() {
+    if (timeLeft < 6) {
+      time.style.color = "red";
+    }
+    if (timeLeft > 0) {
+      time.innerHTML = "Time: " + timeLeft;
+      timeLeft--;
+    } else {
+      timesUp(timerId);
+    }
   }
+}
+
+function timesUp(timerId) {
+  clearTimeout(timerId);
+  time.innerHTML = "Time: 0";
+  quizScreen.style.display = "none";
+  endScreen.style.display = "flex";
+
+  viewScoresLink.style.visibility = "visible";
 }
 
 // Moves from beginning screen and starts the quiz
 beginQuiz.addEventListener("click", (e) => {
+  startTimer();
   openingScreen.style.display = "none";
   viewScoresLink.style.visibility = "hidden";
   quizScreen.style.display = "flex";
+  time.style.visibility = "visible";
 });
 
 // Moves from start screen to high score page
